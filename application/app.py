7a042a6971372c5383aca195c014897ed73128d4
twicheg <Twicheg@gmail.com>
2025-02-27 16:51:01 +0300

    add auxiliary functions

--- application/app.py
+++ application/app.py
@@ -1,14 +1,11 @@
-
 from fastapi import FastAPI, Request
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
 from fastapi.responses import HTMLResponse
-from sqlalchemy import select
 
 from application.database import create_tables, get_session, SessionDep
 from application.models import Users, Countries
 from application.routers import names_router
-from application.schemas import UserSchema, UserSchemaResponse
 
 app = FastAPI()
 
@@ -24,10 +21,8 @@ app.include_router(names_router)
 templates = Jinja2Templates(directory="templates")
 
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/", response_class=HTMLResponse, tags=["web"])
 async def read_item(request: Request, session: SessionDep):
     return templates.TemplateResponse(
         request=request, name="index.html", context={}
     )
-
-
--- application/routers.py
+++ application/routers.py
@@ -1,25 +1,36 @@
-from typing import Annotated, List
+from typing import List, Annotated
 
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
-
+from fastapi import APIRouter, Query, status, HTTPException
 from application.database import SessionDep
 from application.models import Users, Countries
 from application.schemas import UserSchema, UserSchemaResponse
+from application.service import get_user_from_request, add_to_database
 
 names_router = APIRouter(prefix="/names")
 
 
-@names_router.get("/", response_model=List[UserSchemaResponse])
-async def get_users(session: SessionDep, name: str = ''):
+@names_router.get("/", status_code=200, tags=['user'], response_model=List[UserSchemaResponse])
+async def get_users(session: SessionDep,
+                    name: Annotated[str | None, Query(max_length=20)] = None,
+                    url: str | None = None):
     if name:
-        return session.query(Users).filter(Users.name == name)
+        user_query = session.query(Users).filter(Users.name == name)
+        users_with_that_name = [user for user in user_query]
+        if users_with_that_name:
+            return user_query
+
+        else:
+            user = await get_user_from_request(name, url)
+            if user:
+                user = await add_to_database(user, session)
+                return [user]
+            else:
+                raise HTTPException(status_code=404, detail="Not found")
+
     return session.query(Users).all()
 
 
-@names_router.post('/', status_code=201, response_model=UserSchemaResponse)
-async def create_user(user: UserSchema, session: Depends(SessionDep)):
-    user = Users(name=user.name, country=[Countries(**i.dict()) for i in user.country])
-    session.add(user)
-    session.commit()
+@names_router.post('/', status_code=201, tags=['user'], response_model=UserSchemaResponse)
+async def create_user(user: UserSchema, session: SessionDep):
+    user = await add_to_database(user, session)
     return user
