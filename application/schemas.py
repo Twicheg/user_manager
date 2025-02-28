d7409919c7e145001f745b7b431488f87d7d9eca
twicheg <Twicheg@gmail.com>
2025-02-28 20:01:58 +0300

    write template's post , add validators

--- application/schemas.py
+++ application/schemas.py
@@ -1,19 +1,37 @@
-from pydantic import BaseModel
+from fastapi import HTTPException
+from pydantic import BaseModel, field_validator, field_serializer, model_validator
 from typing import List
+
+from application.models import Users
+
+from application.database import get_session
 
 
 class CountrySchema(BaseModel):
     country_id: str
     probability: float
 
+    @field_validator('country_id', mode="before")
+    @classmethod
+    def check(cls, country_id: str) -> str:
+        if len(country_id) > 5:
+            raise HTTPException(status_code=422, detail="max size of country_id is 5")
+        return country_id
+
 
 class UserSchema(BaseModel):
     name: str
     country: List[CountrySchema] = []
+
+    @field_validator('name', mode="before")
+    @classmethod
+    def check(cls, name: str) -> str:
+        if not name or name in [i.name for i in get_session().__next__().query(Users).all()]:
+            raise HTTPException(status_code=422, detail="User must be unique")
+        return name
 
 
 class UserSchemaResponse(BaseModel):
     counter: int
     name: str
     country: List[CountrySchema] = []
-
