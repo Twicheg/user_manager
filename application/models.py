1851b807a2dd4449de161c6e567264b1ea0ba312
eliseev <eliseev@eliseev-Aspire>
2025-03-03 10:56:59 +0300

    some correct models.py

--- application/models.py
+++ application/models.py
@@ -15,21 +15,20 @@ class Base(DeclarativeBase):
 class Users(Base):
     __tablename__ = "users"
 
-    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    counter: Mapped[int] = id
-    name: Mapped[Optional[str]] = mapped_column(String(50))
+    counter : Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    name: Mapped[Optional[str]] = mapped_column(max_lenght=150,unique=True)
     country: Mapped[Optional[List["Countries"]]] = relationship(
         back_populates="user", cascade="all, delete-orphan")
 
     def __repr__(self) -> str:
-        return f"User(id={self.id}, name={self.name}, country={self.country})"
+        return f"User(id={self.counter}, name={self.name}, country={self.country})"
 
 
 class Countries(Base):
     __tablename__ = "countries"
 
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
-    user_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
+    user_id: Mapped[int] = mapped_column(ForeignKey("users.counter"))
     user: Mapped["Users"] = relationship(back_populates="country")
-    country_id: Mapped[str] = mapped_column(String(5))
-    probability: Mapped[float] = mapped_column(Float())
+    country_id: Mapped[str] = mapped_column(max_lenght=10)
+    probability: Mapped[float] = mapped_column()
